24d97273542a00b5f2a14a1630668dedef8b6c46
lsrom <lukas.srom@gmail.com>
2018-05-27 14:15:05 +0200

    Add view models

---
+++ app/src/main/java/cz/lsrom/deadlyabroutine/viewmodel/MainViewModel.kt
@@ -0,0 +1,10 @@
+package cz.lsrom.deadlyabroutine.viewmodel
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+
+/**
+ * @author Lukas Srom <lukas.srom@gmail.com>
+ */
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+}
---
+++ app/src/main/java/cz/lsrom/deadlyabroutine/viewmodel/RoutineViewModel.kt
@@ -0,0 +1,9 @@
+package cz.lsrom.deadlyabroutine.viewmodel
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * @author Lukas Srom <lukas.srom@gmail.com>
+ */
+class RoutineViewModel : ViewModel() {
+}
