6200d90d2d7ad9da650681397dda18ab868e54a3
lsrom <lukas.srom@gmail.com>
2018-06-02 23:14:57 +0200

    Add time class

---
+++ app/src/main/java/cz/lsrom/deadlyabroutine/model/Time.kt
@@ -0,0 +1,36 @@
+package cz.lsrom.deadlyabroutine.model
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.os.Parcel
+import android.os.Parcelable
+
+/**
+ * @author Lukas Srom <lukas.srom@gmail.com>
+ */
+data class Time(var minutes: Int, var seconds: Int) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(minutes)
+        parcel.writeInt(seconds)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Time> {
+        override fun createFromParcel(parcel: Parcel): Time {
+            return Time(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Time?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
